8638016bfc937fe2b79274f26fa40bc411585413
leedd87 <leedav87@gmail.com>
2024-05-27 15:16:51 -0300

    projects config

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ProductsApp } from './src/ProductsApp';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <ProductsApp />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/ProductsApp.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export const ProductsApp = () => {
+  return (
+    <View>
+      <Text>ProductsApp</Text>
+    </View>
+  );
+};
