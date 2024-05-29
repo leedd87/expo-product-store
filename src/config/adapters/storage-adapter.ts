1e342150add0f1d1d0edc847d7d3f4fc99737365
leedd87 <leedav87@gmail.com>
2024-05-29 13:54:05 -0300

    add storage adapter

---
+++ src/config/adapters/storage-adapter.ts
@@ -0,0 +1,29 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export class StorageAdapter {
+  static async getItem(key: string): Promise<string | null> {
+    try {
+      const value = await AsyncStorage.getItem(key);
+      return value;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  static async setItem(key: string, value: string): Promise<void> {
+    try {
+      await AsyncStorage.setItem(key, value);
+    } catch (error) {
+      throw new Error(`Error setting item ${key} and ${value}`);
+    }
+  }
+
+  static async removeItem(key: string): Promise<void> {
+    try {
+      await AsyncStorage.removeItem(key);
+    } catch (error) {
+      console.log(error);
+      throw new Error(`Error removing item ${key}`);
+    }
+  }
+}
